e624d338760fe3d97a2e91048a42087db1da5b64
juandiazrodriguez <juan.diaz.7e3@itb.cat>
2021-05-18 18:25:42 +0200

    last translations and button disabled commit

--- Guetta-WebApp/src/screens/AboutUs.js
+++ Guetta-WebApp/src/screens/AboutUs.js
@@ -53,17 +53,14 @@ function AboutUs() {
         </div> 
       </div>
       <img src={logo} className="logoHorizontal" alt="logo" />
-      <h2>ABOUT US</h2><br/>
+      <h2>{t("words.aboutus")}</h2><br/>
       <h5 className="text">
-      With Guetta you have at your fingertips the possibility of getting all the sounds of your
-favorite songs.<br/><br/>
+      {t("words.text-about-us1")}<br/><br/>
 
-Millions of people around the world feel limited when it comes to
-create your own musical projects based on different songs. Guetta
-It will help them to do their new songs with a high quality.<br/><br/>
+      {t("words.text-about-us2")}<br/><br/>
 
-Our objective is to provide music by and for everyone. What are you waiting for
-enjoy Guetta?<br/><br/><br/><br/><br/><br/><br/>
+      {t("words.text-about-us3")}
+<br/><br/><br/><br/><br/><br/><br/>
       </h5>
       <div className="parent2">
         <Link className="terms" to="./terms">{t("words.terms")}</Link>
