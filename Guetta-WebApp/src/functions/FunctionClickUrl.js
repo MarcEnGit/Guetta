f5072b25ae2ba2c5ebe136831e4d2409ab4dfdde
AgustiRodelasITB <81176927+AgustiRodelasITB@users.noreply.github.com>
2021-05-18 16:49:25 +0200

    Update FunctionClickUrl.js

--- Guetta-WebApp/src/functions/FunctionClickUrl.js
+++ Guetta-WebApp/src/functions/FunctionClickUrl.js
@@ -15,7 +15,7 @@ function FunctionClickURL() {
         formData.append("urlText", urlText);
         try {
             const res = await axios.post(
-                "http://localhost:3002/ytconvert",
+                "http://35.205.50.110:3002/ytconvert",
                 formData
             );
             console.log(res.data);
