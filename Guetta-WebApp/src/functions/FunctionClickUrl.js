63baa671bd9d414e320cd691a4cbd175a0390d86
SergiMolinaItb <56590337+SergiMolinaItb@users.noreply.github.com>
2021-05-28 16:34:19 +0200

    Comment FunctionClickURL.js Commit - Sergi

--- Guetta-WebApp/src/functions/FunctionClickUrl.js
+++ Guetta-WebApp/src/functions/FunctionClickUrl.js
@@ -4,13 +4,22 @@ import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
+// Arxiu JS per pujar un arxiu.
 function FunctionClickURL() {
+    
+    // Constants per guardar la URL.
     const [url, setUrl] = useState('');
+    
+    // Constant per canviar l'idioma del text per cada idioma que té la web.    
     const [t] = useTranslation("global");
+    
+    // Constant per redirigir d'una pàgina a una altre.
     const history = useHistory();
 
+    // Constant per habilitar i deshabilitar el botó d'enviar l'arxiu mentre les funcions s'estàn processant.
     const [disable, setDisable] = useState(false);
 
+    // Funció per pujar l'URL a l'API.
     const uploadUrl = async (e) => {
         setDisable(true)
         console.log('press')
@@ -35,15 +44,15 @@ function FunctionClickURL() {
         }
         setDisable(false)
     }
-
+    
+    // Funció que, quan es pressiona la tecla "ENTER" a l'input text, la funció de pujar el link també s'executa.
     const handleKeypress = e => {
         if (e.keyCode === 13) {
             uploadUrl()
         }
     };
 
-
-
+    // HTML que s'implementa a l'executar la URL.
     return (
         <div className="divFunction">
             <input id="urlText" className="inputURL" type="text" placeholder={t("words.button-url")} onKeyDown={handleKeypress} />
@@ -53,4 +62,5 @@ function FunctionClickURL() {
         </div>
     )
 }
+
 export default FunctionClickURL
